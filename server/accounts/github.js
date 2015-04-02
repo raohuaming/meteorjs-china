3655711bc44b67c13c355a2e43b6f1744f99d1cc
wenhuang98 <wenhuang98@gmail.com>
2015-04-02 19:00:26 +0800

    allow user to login with github

---
+++ server/accounts/github.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+  service: "github"
+});
+
+ServiceConfiguration.configurations.insert({
+  service: "github",
+  clientId: "dea94f0d280ea68f9478",
+  secret: "3e10c51d42662e03e7d75648ecac31d5fb82b433"
+});
